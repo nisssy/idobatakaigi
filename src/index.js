971d1fe796db7252d7b4ea35062cf35353d2bb18
nisssy <kazuhiro.nishikawa.96@gmail.com>
2021-01-11 16:37:30 +0900

    Sign In Page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import App from './App';
+import App from './components/App';
+import SignIn from './components/SignIn';
 
 ReactDOM.render(
   <React.StrictMode>
