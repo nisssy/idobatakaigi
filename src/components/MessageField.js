0f0febe84cb7b2bd200dfc1fd7ce3747d84438dd
nisssy <kazuhiro.nishikawa.96@gmail.com>
2021-01-14 14:04:39 +0900

    remove hard coded enve vars

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import { TextField } from '@material-ui/core';
-import {pushMessage} from './firebase.js';
+import {pushMessage} from '../firebase';
 
 const MessageField = ({name, setText, text}) => {
     const [isComposed, setIsComposed] = useState(false);
